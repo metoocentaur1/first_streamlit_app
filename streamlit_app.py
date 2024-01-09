72bdb61afee5ddbb659d25b059a44330b45569d8
metoocentaur1 <156129039+metoocentaur1@users.noreply.github.com>
2024-01-09 13:01:17 -0700

    Update streamlit_app.py
    moving fruit list

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_fruit_list)
+
 
 streamlit.title('My parents new healthy diner')
 streamlit.header('Breakfast Favorites')
@@ -10,3 +10,6 @@ streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+
+streamlit.dataframe(my_fruit_list)
