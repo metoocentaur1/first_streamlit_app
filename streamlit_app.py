023a5d913bbfb243ee7b4e43bf10b60f4d8be9c6
metoocentaur1 <156129039+metoocentaur1@users.noreply.github.com>
2024-01-11 10:00:35 -0700

    Update streamlit_app.py
    troubleshooting

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,46 +49,6 @@ except URLError as e:
   streamlit.error()
 
 
-#New Section to display fruityvice api response
-#streamlit.header("Fruityvice Fruit Advice!")
-#try:
-#  fruit_choice = streamlit.text_input("What fruit would you like information about?")
-#  if not fruit_choice:
- #   streamlit.error("Please select a fruit to get information.")
-#  else:
-  #  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-  #  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-  #  streamlit.dataframe(fruityvice_normalized)
-
-
-
-
-
-
-
-
-
-
-
-#streamlit.write("The user entered", fruit_choice)
-
-#############  Below code moved above into the else statement  #################
-#requests being used here
-#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-
-#Take the json version of the response and normalize it
-#fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-#output it to the screen as a table
-#streamlit.dataframe(fruityvice_normalized)
-
-
-
-#Snowflake being used here
-#my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-#my_cur = my_cnx.cursor()
-#my_cur.execute("SELECT * from fruit_load_list")
-#my_data_rows = my_cur.fetchall()
 
 
 streamlit.header("The fruit load list contains:")
@@ -99,10 +59,10 @@ def get_fruit_load_list():
     return my_cur.fetchall()
 
 #Add a button to load the fruit
-
 if streamlit.button("Get fruit load list"):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_rows = get_fruit_load_list()
+  my_cnx.close()
   streamlit.dataframe(my_data_rows) 
 
 
